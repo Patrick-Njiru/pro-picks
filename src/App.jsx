8a6bf4809518b74610e70b7707f9c038ce5b1f75
Patrick Njiru <patricknjiru93@gmail.com>
2024-02-17 02:00:11 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+export default App
